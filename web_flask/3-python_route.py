9517cb5de083fa65ea1e9896c7934ff73065493a
gabbrenn <gabvladimirbrenn@gmail.com>
2024-03-23 18:22:15 +0000

    UPdate

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+#route definition
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+	"""Display Hello HBNB"""
+	return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+	"""Display HBNB"""
+	return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def dynamic_value(text):
+	"""Display Dynamic Content"""
+	return f'C {escape(text)}'
+
+@app.route("/python/<text>", strict_slashes=False)
+def py_values(text = "is cool"):
+	"""Display Python is cool"""
+	return f'Python {escape(text)}'
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000)
