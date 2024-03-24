825c21d9bc30702573912f1d093fa875de8c0fca
Gabiro Vladimir Brenn <103499307+gabbrenn@users.noreply.github.com>
2024-03-24 19:30:47 +0200

    Update

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,36 +1,41 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+start Flask application
 """
+
 from flask import Flask
-from markupsafe import escape
 app = Flask(__name__)
 
-#route definition
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-	"""Display Hello HBNB"""
-	return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-	"""Display HBNB"""
-	return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
-@app.route("/c/<text>", strict_slashes=False)
-def dynamic_value(text):
-	"""Display Dynamic Content"""
-	return 'C ' + text.replace('_', ' ')
 
-@app.route("/python/<text>", strict_slashes=False)
-def py_values(text = "is cool"):
-	"""Display Python is cool"""
-	return 'Python ' + text.replace('_', ' ')
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def imanumber(n):
-    """display n is a number only if n is an integer"""
+    """display “n is a number” only if n is an integer"""
     return "{:d} is a number".format(n)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
